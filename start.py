099cb415a7348b2ff3473d900922ba9579ae8efd
张仁 <wenzhu_work@163.com>
2021-08-20 23:30:24 +0800

    修改配置文件以及程序入口

--- start.py
+++ start.py
@@ -9,9 +9,10 @@
 """
 from __future__ import unicode_literals, division, print_function, absolute_import
 
-import argparse
 import io
+import os
 import sys
+from configparser import ConfigParser
 
 import pkg_resources
 from sqlalchemy.engine import create_engine
@@ -19,30 +20,59 @@ from sqlalchemy.schema import MetaData
 
 from sqlacodegen.codegen import CodeGenerator
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_DIR = os.path.join(BASE_DIR, 'config')
+CONFIG = ConfigParser()
+CONFIG.read(os.path.join(CONFIG_DIR, 'config.ini'), encoding='utf-8')
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Generates SQLAlchemy model code from an existing database.')
 
-    args = parser.parse_args()
+def main():
+    url = CONFIG['PARAM']['URL'] if CONFIG['PARAM']['URL'] else None
+    # version = CONFIG['PARAM']['VERSION'] if CONFIG['PARAM']['VERSION'] else None
+    # schema = CONFIG['PARAM']['SCHEMA'] if CONFIG['PARAM']['SCHEMA'] else None
+    tables = CONFIG['PARAM']['TABLES'] if CONFIG['PARAM']['TABLES'] else None
+    # noviews = CONFIG['PARAM']['NOVIEWS'] if CONFIG['PARAM']['NOVIEWS'] else None
+    # noindexes = CONFIG['PARAM']['NOINDEXES'] if CONFIG['PARAM']['NOINDEXES'] else None
+    # noconstraints = CONFIG['PARAM']['NOCONSTRAINTS'] if CONFIG['PARAM']['NOCONSTRAINTS'] else None
+    # nojoined = CONFIG['PARAM']['NOJOINED'] if CONFIG['PARAM']['NOJOINED'] else None
+    # noinflect = CONFIG['PARAM']['NOINFLECT'] if CONFIG['PARAM']['NOINFLECT'] else None
+    # noclasses = CONFIG['PARAM']['NOCLASSES'] if CONFIG['PARAM']['NOCLASSES'] else None
+    # nocomments = CONFIG['PARAM']['NOCOMMENTS'] if CONFIG['PARAM']['NOCOMMENTS'] else None
+    outfile = CONFIG['PARAM']['OUTFILE'] if CONFIG['PARAM']['OUTFILE'] else None
 
-    if args.version:
+    if None:
         version = pkg_resources.get_distribution('sqlacodegen').parsed_version
         print(version.public)
         return
-    if not args.url:
+
+    if not url:
         print('You must supply a url\n', file=sys.stderr)
-        parser.print_help()
         return
 
-    # Use reflection to fill in the metadata
-    engine = create_engine(args.url)
+        # Use reflection to fill in the metadata
+    engine = create_engine(url)
     metadata = MetaData(engine)
-    tables = args.tables.split(',') if args.tables else None
-    metadata.reflect(engine, args.schema, not args.noviews, tables)
+    tables = tables.split(',') if tables else None
+
+    # 临时创建的比变量：
+    schema = None
+    noviews = False
+    noindexes = False
+    noconstraints = False
+    nojoined = False
+    noinflect = False
+    noclasses = False
+    nocomments = False
+    metadata.reflect(engine, schema, not noviews, tables)
 
     # Write the generated model code to the specified file or standard output
-    outfile = io.open(args.outfile, 'w', encoding='utf-8') if args.outfile else sys.stdout
-    generator = CodeGenerator(metadata, args.noindexes, args.noconstraints, args.nojoined,
-                              args.noinflect, args.noclasses, nocomments=args.nocomments)
+    outfile = io.open(outfile, 'w', encoding='utf-8') if outfile else sys.stdout
+    generator = CodeGenerator(metadata, noindexes, noconstraints, nojoined,
+                              noinflect, noclasses, nocomments=nocomments)
+
+    print("successful")
     generator.render(outfile)
+
+
+if __name__ == '__main__':
+    main()
