ff49058717d9ce499db5a6a346365ac8f5a65892
jackiex <xjg@ncepu.edu.cn>
2021-08-21 16:42:53 +0800

    增加配置项

---
+++ sqlacodegen/staticcodegen/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:main.py
+# author:jackiex
+# datetime:2021/8/21 16:38
+# software: PyCharm
+'''
+    this is function  description 
+'''
+# import module your need
