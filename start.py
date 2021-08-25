f8cac234ddd1780461ea8345d034039ebc81a3a7
Nathan <wenzhustudy@gmail.com>
2021-08-25 14:17:32 +0800

    model层生成代码提交

--- start.py
+++ start.py
@@ -11,14 +11,16 @@
 """
 
 from codegen.controllercodegen import controllerGenerate
-from codegen.modelcodegen import modelGenerate
+from codegen.modelcodegen_new import modelGenerate
 from codegen.resourcecodegen import resourceGenerate
 from codegen.staticcodegen import staticGenerate
+from utils.checkConfig import check_config
 from utils.loggings import loggings
 
 if __name__ == '__main__':
     """
         步骤：
+            零、 配置文件参数检查
             一、 生成Model层代码
             二、 生成Controller层代码
             三、 生成Resource层代码
@@ -26,10 +28,12 @@ if __name__ == '__main__':
             五、 目标项目启动
             六、 目标项目接口测试
     """
+    # 第零步
+    check_config()
 
     # 第一步
     loggings.info(1, "开始构建Model层代码, 请稍等...")
-    # modelGenerate()
+    modelGenerate()
     loggings.info(1, "构建Model层代码完成")
 
     # 第二步
