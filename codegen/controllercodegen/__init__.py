7a44bac386e4aa713013cfc93b68e542b800f21c
Itsuka <1174712335@qq.com>
2021-08-26 15:21:18 +0800

    参数读取与主函数分离

--- codegen/controllercodegen/__init__.py
+++ codegen/controllercodegen/__init__.py
@@ -7,5 +7,21 @@
 # software: PyCharm
 
 """
-    this is function  description
+    load settings and init
 """
+from sqlalchemy import create_engine, MetaData
+
+from config.setting import Settings
+from utils.checkTable import CheckTable
+
+url = Settings.MODEL_URL
+codegen_layer = Settings.CODEGEN_LAYER
+schema = Settings.MODEL_SCHEMA
+noviews = Settings.MODEL_NOVIEWS
+record_delete_way = Settings.CONTROLLER_RECORD_DELETE_WAY
+tables = CheckTable.check_primary_key()
+
+# link to the database and get metadata by reflection
+engine = create_engine(url)
+metadata = MetaData(engine)
+metadata.reflect(engine, schema, not noviews, tables)
--- codegen/controllercodegen/main.py
+++ codegen/controllercodegen/main.py
@@ -11,13 +11,11 @@
 """
 import os
 
-import pkg_resources
-from sqlalchemy import create_engine, MetaData
-
 from config.setting import Settings
 from utils.checkTable import CheckTable
 from utils.common import new_file_or_dir
 from .codegenerator import CodeGenerator
+from . import codegen_layer, metadata, record_delete_way
 
 
 def controllerGenerate():
@@ -25,36 +23,20 @@ def controllerGenerate():
     Generate Controller code
     :return: None
     """
-    url = Settings.MODEL_URL
-    codegen_layer = Settings.CODEGEN_LAYER
-    schema = Settings.MODEL_SCHEMA
-    version = Settings.MODEL_VERSION
-    noviews = Settings.MODEL_NOVIEWS
-    record_delete_way = Settings.CONTROLLER_RECORD_DELETE_WAY
 
     # return, while codegen_layer is not 'default' or 'controller'
     if codegen_layer not in ['default', 'controller']:
         return
 
-    if version:
-        version = pkg_resources.get_distribution('sqlacodegen').parsed_version
-        print(version.public)
-        return
-
     tables = CheckTable.check_primary_key()
-    if tables is None:
+    if not tables:
         return
 
-    # get the target dir
+    # create the target dir
     new_file_or_dir(2, Settings.TARGET_DIR)
     new_file_or_dir(2, Settings.PROJECT_DIR)
     controller_dir = os.path.join(Settings.PROJECT_DIR, 'controller')
     new_file_or_dir(2, controller_dir)
-
-    # link to the database and get metadata by reflection
-    engine = create_engine(url)
-    metadata = MetaData(engine)
-    metadata.reflect(engine, schema, not noviews, tables)
 
     generator = CodeGenerator(metadata)
     generator.controller_codegen(delete_way=record_delete_way, controller_dir=controller_dir)
