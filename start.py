621c93c4e026b4777b5c79bdb8d4336c3db43bc3
TzPig <1731318334@qq.com>
2021-12-17 12:58:30 +0800

    ui界面修改

--- start.py
+++ start.py
@@ -14,6 +14,5 @@ import webbrowser
 from ui.api.views import app
 
 if __name__ == '__main__':
-    # webbrowser.open('http://127.0.0.1:5000')
-    os.open("vue_ui/index.html")
+    webbrowser.open('http://127.0.0.1:5000')
     app.run()
--- ui/api/views.py
+++ ui/api/views.py
@@ -12,153 +12,178 @@
 
 import ast
 import json
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import configparser
 from utils.checkSqlLink import check_sql_link
 
-app = Flask(__name__, template_folder="../../vue_ui", static_folder="../static")
+app = Flask(__name__, static_folder="../static")
 Bootstrap(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         dialect = request.form.get("dialect")
+#         driver = request.form.get("driver")
+#         host = request.form.get("host")
+#         port = request.form.get("port")
+#         database = request.form.get("database")
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#
+#         # tables 为空
+#         configfile = "config/config.conf"
+#         conf = configparser.ConfigParser()
+#         conf.read(configfile, encoding='UTF-8')
+#         conf.set("MODEL", "TABLES", '')
+#         with open(configfile, "w") as f:
+#             conf.write(f)
+#         # 检查数据库链接
+#         result_sql = check_sql_link(dialect, driver, username, password, host, port, database)
+#         if result_sql['code']:
+#             global tabledata
+#             tabledata = result_sql['data']
+#             # 填写配置文件
+#             configfile = "config/database.conf"
+#             conf = configparser.ConfigParser()  # 实例类
+#             conf.read(configfile, encoding='UTF-8')  # 读取配置文件
+#             conf.set("DEFAULT", "DIALECT", dialect)  # 第一个参数为组名，第二个参数为属性名，第三个参数为属性的值
+#             conf.set("DEFAULT", "DRIVER", driver)
+#             conf.set("DEFAULT", "HOST", host)
+#             conf.set("DEFAULT", "PORT", port)
+#             conf.set("DEFAULT", "DATABASE", database)
+#             conf.set("DEFAULT", "USERNAME", username)
+#             conf.set("DEFAULT", "PASSWORD", password)
+#             with open(configfile, "w") as f:
+#                 conf.write(f)
+#             return redirect(url_for('table'))
+#
+#         else:
+#             return render_template("index.html", message=result_sql['message'])
+#     return render_template("index.html")
+
+
+# @app.route('/table')
+# def table():
+#     global tabledata
+#     return render_template("table.html", data=tabledata)
+#
+#
+# @app.route('/project', methods=['GET', 'POST'])
+# def project():
+#     if request.method == 'POST':
+#         projectPath = request.form.get("projectPath")
+#         projectName = request.form.get("projectName")
+#         interfaceVersion = request.form.get("interfaceVersion")
+#
+#         configfile = "config/config.conf"
+#         conf = configparser.ConfigParser()  # 实例类
+#         conf.read(configfile, encoding='UTF-8')  # 读取配置文件
+#
+#         conf.set("PARAMETER", "TARGET_DIR", projectPath)  # 第一个参数为组名，第二个参数为属性名，第三个参数为属性的值
+#         conf.set("PARAMETER", "PROJECT_NAME", projectName)
+#         conf.set("PARAMETER", "API_VERSION", interfaceVersion)
+#         with open(configfile, "w") as f:
+#             conf.write(f)
+#         return redirect(url_for("showtableinfo"))
+#     return render_template("project.html")
+#
+#
+# @app.route('/build', methods=['GET'])
+# def build():
+#     from codegen.main import start
+#     start()
+#     with open('logs/codegen_log.log', "r", encoding="utf-8") as f:
+#         log_data = f.read()
+#
+#     return render_template("build.html", log_data=log_data)
+#
+#
+# @app.route('/tableinfo/<tableinfo>')
+# def tableinfo(tableinfo):
+#     global tabledata
+#     tabledata = ast.literal_eval(tableinfo)
+#     security_configfile = "config/security.conf"
+#     security_conf = configparser.ConfigParser()  # 实例类
+#     security_conf.read(security_configfile, encoding='UTF-8')  # 读取配置文件
+#     security_conf.remove_section("RSA_TABLE_COLUMN")
+#     security_conf.add_section("RSA_TABLE_COLUMN")
+#
+#     config_configfile = "config/config.conf"
+#     config_config = configparser.ConfigParser()  # 实例类
+#     config_config.read(config_configfile, encoding='UTF-8')  # 读取配置文件
+#
+#     table_rule = {
+#         "table_record_delete_logic_way": [
+#         ],
+#         "table_business_key_gen_rule": {
+#         }
+#     }
+#
+#     tables_str = ""
+#     for tableItem in tabledata:
+#         if tableItem['issave'] == 'true':
+#             tables_str = tables_str + tableItem['table'] + ","
+#             config_config.set("MODEL", 'TABLES', tables_str[:-1])
+#             with open(config_configfile, "w") as f:
+#                 config_config.write(f)
+#
+#             if tableItem['encrypt'] != []:
+#                 encrypt_str = ""
+#                 for str in tableItem['encrypt']:
+#                     encrypt_str = encrypt_str + str + ","
+#                 security_conf.set("RSA_TABLE_COLUMN", tableItem['table'], encrypt_str[:-1])
+#                 with open(security_configfile, "w") as f:
+#                     security_conf.write(f)
+#
+#             if tableItem['isdeleted'] == 'true':
+#                 table_rule['table_record_delete_logic_way'].append(tableItem['table'])
+#
+#             if tableItem['isbusinesskey'] != '':
+#                 table_rule['table_business_key_gen_rule'][tableItem['table']] = {
+#                     tableItem['isbusinesskey']: tableItem['businesskeyrule']}
+#
+#     table_rule_json = json.dumps(table_rule)
+#     with open("config/table_rule.json", "w") as f:
+#         f.write(table_rule_json)
+#
+#     return redirect(url_for('project'))
+#
+#
+# @app.route('/showtableinfo', methods=['GET', 'POST'])
+# def showtableinfo():
+#     if request.method == 'POST':
+#         return render_template("build.html")
+#     return render_template("showTableInfo.html", tabledata=tabledata)
+
+
+# 新的vue接口
+
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        dialect = request.form.get("dialect")
-        driver = request.form.get("driver")
-        host = request.form.get("host")
-        port = request.form.get("port")
-        database = request.form.get("database")
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        # tables 为空
-        configfile = "config/config.conf"
-        conf = configparser.ConfigParser()
-        conf.read(configfile, encoding='UTF-8')
-        conf.set("MODEL", "TABLES", '')
-        with open(configfile, "w") as f:
-            conf.write(f)
-        # 检查数据库链接
-        result_sql = check_sql_link(dialect, driver, username, password, host, port, database)
-        if result_sql['code']:
-            global tabledata
-            tabledata = result_sql['data']
-            # 填写配置文件
-            configfile = "config/database.conf"
-            conf = configparser.ConfigParser()  # 实例类
-            conf.read(configfile, encoding='UTF-8')  # 读取配置文件
-            conf.set("DEFAULT", "DIALECT", dialect)  # 第一个参数为组名，第二个参数为属性名，第三个参数为属性的值
-            conf.set("DEFAULT", "DRIVER", driver)
-            conf.set("DEFAULT", "HOST", host)
-            conf.set("DEFAULT", "PORT", port)
-            conf.set("DEFAULT", "DATABASE", database)
-            conf.set("DEFAULT", "USERNAME", username)
-            conf.set("DEFAULT", "PASSWORD", password)
-            with open(configfile, "w") as f:
-                conf.write(f)
-            return redirect(url_for('table'))
-
-        else:
-            return render_template("index.html", message=result_sql['message'])
-    return render_template("index.html")
+    return app.send_static_file('index.html')
 
 
-@app.route('/table')
-def table():
-    global tabledata
-    return render_template("table.html", data=tabledata)
+@app.route('/tables', methods=['GET'])
+def tables():
+    return app.send_static_file('tables.html')
 
 
-@app.route('/project', methods=['GET', 'POST'])
+@app.route('/tables_info', methods=['GET'])
+def tables_info():
+    return app.send_static_file('tables_info.html')
+
+
+@app.route('/project', methods=['GET'])
 def project():
-    if request.method == 'POST':
-        projectPath = request.form.get("projectPath")
-        projectName = request.form.get("projectName")
-        interfaceVersion = request.form.get("interfaceVersion")
-
-        configfile = "config/config.conf"
-        conf = configparser.ConfigParser()  # 实例类
-        conf.read(configfile, encoding='UTF-8')  # 读取配置文件
-
-        conf.set("PARAMETER", "TARGET_DIR", projectPath)  # 第一个参数为组名，第二个参数为属性名，第三个参数为属性的值
-        conf.set("PARAMETER", "PROJECT_NAME", projectName)
-        conf.set("PARAMETER", "API_VERSION", interfaceVersion)
-        with open(configfile, "w") as f:
-            conf.write(f)
-        return redirect(url_for("showtableinfo"))
-    return render_template("project.html")
+    return app.send_static_file('project.html')
 
 
 @app.route('/build', methods=['GET'])
 def build():
-    from codegen.main import start
-    start()
-    with open('logs/codegen_log.log', "r", encoding="utf-8") as f:
-        log_data = f.read()
+    return app.send_static_file('build.html')
 
-    return render_template("build.html", log_data=log_data)
-
-
-@app.route('/tableinfo/<tableinfo>')
-def tableinfo(tableinfo):
-    global tabledata
-    tabledata = ast.literal_eval(tableinfo)
-    security_configfile = "config/security.conf"
-    security_conf = configparser.ConfigParser()  # 实例类
-    security_conf.read(security_configfile, encoding='UTF-8')  # 读取配置文件
-    security_conf.remove_section("RSA_TABLE_COLUMN")
-    security_conf.add_section("RSA_TABLE_COLUMN")
-
-    config_configfile = "config/config.conf"
-    config_config = configparser.ConfigParser()  # 实例类
-    config_config.read(config_configfile, encoding='UTF-8')  # 读取配置文件
-
-    table_rule = {
-        "table_record_delete_logic_way": [
-        ],
-        "table_business_key_gen_rule": {
-        }
-    }
-
-    tables_str = ""
-    for tableItem in tabledata:
-        if tableItem['issave'] == 'true':
-            tables_str = tables_str + tableItem['table'] + ","
-            config_config.set("MODEL", 'TABLES', tables_str[:-1])
-            with open(config_configfile, "w") as f:
-                config_config.write(f)
-
-            if tableItem['encrypt'] != []:
-                encrypt_str = ""
-                for str in tableItem['encrypt']:
-                    encrypt_str = encrypt_str + str + ","
-                security_conf.set("RSA_TABLE_COLUMN", tableItem['table'], encrypt_str[:-1])
-                with open(security_configfile, "w") as f:
-                    security_conf.write(f)
-
-            if tableItem['isdeleted'] == 'true':
-                table_rule['table_record_delete_logic_way'].append(tableItem['table'])
-
-            if tableItem['isbusinesskey'] != '':
-                table_rule['table_business_key_gen_rule'][tableItem['table']] = {
-                    tableItem['isbusinesskey']: tableItem['businesskeyrule']}
-
-    table_rule_json = json.dumps(table_rule)
-    with open("config/table_rule.json", "w") as f:
-        f.write(table_rule_json)
-
-    return redirect(url_for('project'))
-
-
-@app.route('/showtableinfo', methods=['GET', 'POST'])
-def showtableinfo():
-    if request.method == 'POST':
-        return render_template("build.html")
-    return render_template("showTableInfo.html", tabledata=tabledata)
-
-
-# 新的vue接口
 
 # 连接数据库接口
 @app.route('/connect', methods=['POST'])
@@ -203,8 +228,8 @@ def connect():
 
 
 # 完成表配置
-@app.route('/tablesinfo', methods=['POST'])
-def tablesinfo():
+@app.route('/settables', methods=['POST'])
+def settables():
     tabledata = json.loads(request.data)
     security_configfile = "config/security.conf"
     security_conf = configparser.ConfigParser()  # 实例类
@@ -248,8 +273,39 @@ def tablesinfo():
     table_rule_json = json.dumps(table_rule)
     with open("config/table_rule.json", "w") as f:
         f.write(table_rule_json)
-
     return {'code': '2000', 'data': [], 'message': '写入配置成功'}
+
+
+# 完成项目配置
+@app.route('/setproject', methods=['POST'])
+def setproject():
+    kwargs = json.loads(request.data)
+    projectPath = kwargs["projectPath"]
+    projectName = kwargs["projectName"]
+    interfaceVersion = kwargs["projectVersion"]
+
+    configfile = "config/config.conf"
+    conf = configparser.ConfigParser()  # 实例类
+    conf.read(configfile, encoding='UTF-8')  # 读取配置文件
+
+    conf.set("PARAMETER", "TARGET_DIR", projectPath)  # 第一个参数为组名，第二个参数为属性名，第三个参数为属性的值
+    conf.set("PARAMETER", "PROJECT_NAME", projectName)
+    conf.set("PARAMETER", "API_VERSION", interfaceVersion)
+    with open(configfile, "w") as f:
+        conf.write(f)
+    return {'code': '2000', 'data': [], 'message': '写入配置成功'}
+
+@app.route('/startbuild', methods=['POST'])
+def startbuild():
+    from codegen.main import start
+    try:
+        start()
+        with open('logs/codegen_log.log', "r", encoding="utf-8") as f:
+            log_data = f.read()
+        return {'code': '2000', 'data': log_data, 'message': '写入配置成功'}
+    except Exception as e:
+        return {'code': '5000', 'data': [], 'message': '生成失败'}
+
 
 
 @app.after_request
