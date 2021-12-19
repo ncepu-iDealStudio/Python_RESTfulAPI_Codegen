d9f4ec584e8a72bd5ed6d4b463921bf87f599826
Nathan <wenzhustudy@gmail.com>
2021-12-19 15:37:06 +0800

    删除目标代码文件头信息

--- static/__init__.py
+++ static/__init__.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# file:__init__.py.py
-# author:jackiex
-# datetime:2021/8/21 13:15
-# software: PyCharm
-
 """
     this is function  description
 """
