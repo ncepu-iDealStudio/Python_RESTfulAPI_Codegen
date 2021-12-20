7e4ac71f9820f529ba98e506cdce14020e94e0af
Nathan <wenzhustudy@gmail.com>
2021-12-20 16:23:02 +0800

    修改service层的生成代码

--- codegen/servicecodegen/codegenerator.py
+++ codegen/servicecodegen/codegenerator.py
@@ -12,7 +12,6 @@
 
 import os
 
-from codegen import table_dict
 from codegen.servicecodegen.template.codeblocktemplate import CodeBlockTemplate
 from codegen.servicecodegen.template.fileTemplate import FileTemplate
 from utils.common import str_format_convert
@@ -21,89 +20,32 @@ from utils.loggings import loggings
 
 class CodeGenerator(object):
 
-    def __init__(self):
+    def __init__(self, table_dict):
+        self.table_dict = table_dict
         super().__init__()
 
     # resource layer generation
     def service_generator(self, service_path):
         try:
             # Traverse each table to generate the corresponding service layer code
-            for table in table_dict.keys():
+            for table in self.table_dict.keys():
                 loggings.info(1, 'Generating service layer code for "{table_name}" table'.format(
-                    table_name=table_dict[table]['table_name']))
+                    table_name=self.table_dict[table]['table_name']))
 
                 # The name of the table whose code needs to be generated
-                table_name = str_format_convert(table_dict[table]['table_name'])
+                table_name = str_format_convert(self.table_dict[table]['table_name'])
                 table_name_initials_upper = table_name[0].upper() + table_name[1:]
-                Fields_list = []
-                # Database query conditions
-                filter_conditions = '            filter_list.append(cls.IsDelete == 0)\n' if table_dict[table][
-                    'is_logic_delete'] else ""
-
-                # Traverse each column to generate the filter conditions
-                for columns in table_dict[table]['columns']:
-                    columns_name = table_dict[table]['columns'][columns]['name']
-                    # If the primary key mode is "DoubleKey", then AutoID will not be used as a filter condition
-                    if table_dict[table]['business_key'] and columns_name in table_dict[table]['primaryKey']:
-                        continue
-                    filter_conditions += CodeBlockTemplate.single_filter_condition.format(colums_name=columns_name)
-                    Fields_list.append("{table_model}.{columns_name}".format(table_model=table_name_initials_upper,
-                                                                             columns_name=columns_name))
-
-                # If there is a foreign key in the table, add import statement
-                foreign_import = ""
-                # If there is a foreign key in the table, add join target table statement
-                join_table_statement = ""
-                # The string of using underline to combine the name of this table and the associated table
-                result_name = table_name
-
-                # If there is a foreign key in the table, supplement the related template of the associated table
-                if foreign_keys := table_dict[table].setdefault('foreign_keys', None):
-                    for foreign_key in foreign_keys:
-                        # The name of the foreign key associated table (ps. Use CamelCase format)
-                        target_table_name = str_format_convert(foreign_key['target_table'])
-                        # Capitalize the name of the association table
-                        target_table_name_initials_upper = target_table_name[0].upper() + target_table_name[1:]
-                        foreign_import += '\nfrom models.{0}Model import {1}'.format(target_table_name,
-                                                                                     target_table_name_initials_upper)
-                        join_table_statement += CodeBlockTemplate.join_statement.format(
-                            target_table=target_table_name_initials_upper,
-                            table_name_initials_upper=table_name_initials_upper,
-                            table_key=foreign_key['key'],
-                            target_key=foreign_key['target_key'])
-                        result_name += "_" + target_table_name
-
-                        # Fields to be queried
-                        for columns in table_dict[foreign_key['target_table']]['columns']:
-                            columns_name = table_dict[foreign_key['target_table']]['columns'][columns]['name']
-                            Fields_list.append(
-                                "{table_model}.{columns_name}.label('{table_model}.{columns_name}')".format(
-                                    table_model=target_table_name_initials_upper,
-                                    columns_name=columns_name))
-
-                # Fields required for splicing
-                Fields = ', '.join(Fields_list)
 
                 # Format the code block template
                 imports = CodeBlockTemplate.service_import.format(
                     table_name=table_name,
                     table_name_initials_upper=table_name_initials_upper,
-                    foreign_import=foreign_import
                 )
 
                 # Format the file template
                 template = FileTemplate.template.format(
                     imports=imports,
                     table_name_initials_upper=table_name_initials_upper,
-                    foreign_import=foreign_import,
-                    filter_conditions=filter_conditions,
-                    # table_model=table_model,
-                    result_name=result_name + '_info',
-                    Fields=Fields,
-                    join_table_statement=join_table_statement,
-                    exception_return=CodeBlockTemplate.exception_return,
-                    notdata_return=CodeBlockTemplate.notdata_return,
-                    success_return=CodeBlockTemplate.success_return
                 )
 
                 # Write the template to the file
--- codegen/servicecodegen/template/fileTemplate.py
+++ codegen/servicecodegen/template/fileTemplate.py
@@ -22,30 +22,5 @@ class FileTemplate(object):
 {imports}
 
 class {table_name_initials_upper}Service({table_name_initials_upper}Controller):
-    @classmethod
-    def joint_query(cls, **kwargs):
-        try:
-            filter_list = []
-{filter_conditions}          
-            page = int(kwargs.get('Page', 1))
-            size = int(kwargs.get('Size', 10))
-            
-            {result_name} = db.session.query({Fields}){join_table_statement}.filter(*filter_list)
-            
-            count = {result_name}.count()
-            pages = math.ceil(count / size)
-            {result_name} = {result_name}.limit(size).offset((page - 1) * size).all()
-            
-            if not {result_name}:
-                return {notdata_return}
-    
-            # 处理返回的数据
-            results = commons.query_to_dict({result_name})
-            return {success_return}
-        
-        except Exception as e:
-            loggings.exception(1, e)
-            return {exception_return}
-        finally:
-            db.session.close()
+    pass
 """
