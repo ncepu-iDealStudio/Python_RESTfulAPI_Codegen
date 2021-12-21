b8f0a63aeab751815a1502f32ebd237646f8323b
Nathan <wenzhustudy@gmail.com>
2021-12-21 13:07:20 +0800

    代码优化

--- utils/common.py
+++ utils/common.py
@@ -23,25 +23,3 @@ def str_format_convert(string):
         else:
             neo_string = i
     return neo_string
-
-
-# 创建文件或文件夹
-def new_file_or_dir(mode, url):
-    try:
-        if not os.path.exists(url):
-            # new file
-            if mode == 1:
-                file = open(url, 'w')
-                file.close()
-            # new dir
-            elif mode == 2:
-                os.mkdir(url)
-    except Exception as e:
-        loggings.error(1, str(e))
-
-
-# 文件写入
-def file_write(path, content):
-    new_file_or_dir(1, path)
-    with open(path, 'w', encoding='utf8') as f:
-        f.write(content)
