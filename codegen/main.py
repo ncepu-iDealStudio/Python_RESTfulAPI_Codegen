0d1bbe5d986c5b6513f9733fb5345b35090b06ac
Nathan <wenzhustudy@gmail.com>
2021-12-27 17:10:02 +0800

    启动方法添加返回值信息

--- codegen/main.py
+++ codegen/main.py
@@ -20,6 +20,7 @@ import codegen.staticcodegen.main
 import codegen.testcodegen.main
 from codegen import model_url
 from utils.loggings import loggings
+from utils.response_code import RET, error_map
 from utils.tablesMetadata import TableMetadata
 
 
@@ -34,43 +35,56 @@ def start(table_config):
             五、 打包静态文件
             六、 生成test层代码
     """
-    # 参数初始化
-    url = model_url
-    engine = create_engine(url)
-    metadata = MetaData(engine)
+    try:
+        with open('logs/codegen_log.log', 'w', encoding='utf-8') as f:
+            f.seek(0)
+            f.truncate()
 
-    metadata.reflect(engine, only=[table['table'] for table in table_config])
+        # 参数初始化
+        url = model_url
+        engine = create_engine(url)
+        metadata = MetaData(engine)
 
-    table_dict = TableMetadata.get_tables_metadata(metadata, table_config)
+        metadata.reflect(engine, only=[table['table'] for table in table_config])
 
-    # 第一步
-    loggings.info(1, "Start to build the Model layer code, please wait...")
-    codegen.modelcodegen.main.main(table_dict)
-    loggings.info(1, "Model layer code build completed")
+        table_dict = TableMetadata.get_tables_metadata(metadata, table_config)
 
-    # 第二步
-    loggings.info(1, "Start to build the Controller layer code, please wait...")
-    codegen.controllercodegen.main.main(table_dict)
-    loggings.info(1, "Controller layer code build completed")
+        # 第一步
+        loggings.info(1, "Start to build the Model layer code, please wait...")
+        codegen.modelcodegen.main.main(table_dict)
+        loggings.info(1, "Model layer code build completed")
 
-    # 第三步
-    loggings.info(1, "Start to build the Service layer code, please wait...")
-    codegen.servicecodegen.main.main(table_dict)
-    loggings.info(1, "Service layer code build completed")
+        # 第二步
+        loggings.info(1, "Start to build the Controller layer code, please wait...")
+        codegen.controllercodegen.main.main(table_dict)
+        loggings.info(1, "Controller layer code build completed")
 
-    # 第四步
-    loggings.info(1, "Start to build the Resource layer code, please wait...")
-    codegen.resourcecodegen.main.main(table_dict)
-    loggings.info(1, "Resource layer code build completed")
+        # 第三步
+        loggings.info(1, "Start to build the Service layer code, please wait...")
+        codegen.servicecodegen.main.main(table_dict)
+        loggings.info(1, "Service layer code build completed")
 
-    # 第五步
-    loggings.info(1, "Start packing static files, please wait...")
-    codegen.staticcodegen.main.main()
-    loggings.info(1, "Static resource packaging is complete")
+        # 第四步
+        loggings.info(1, "Start to build the Resource layer code, please wait...")
+        codegen.resourcecodegen.main.main(table_dict)
+        loggings.info(1, "Resource layer code build completed")
 
-    # 第六步
-    loggings.info(1, "Start to build the Test layer code, please wait...")
-    codegen.testcodegen.main.main(table_dict)
-    loggings.info(1, "Test layer code build completed")
+        # 第五步
+        loggings.info(1, "Start packing static files, please wait...")
+        codegen.staticcodegen.main.main()
+        loggings.info(1, "Static resource packaging is complete")
 
-    loggings.info(1, "Api project code generation completed")
+        # 第六步
+        loggings.info(1, "Start to build the Test layer code, please wait...")
+        codegen.testcodegen.main.main(table_dict)
+        loggings.info(1, "Test layer code build completed")
+
+        loggings.info(1, "Api project code generation completed")
+
+        with open('logs/codegen_log.log', 'r', encoding='utf-8') as f:
+            result = f.read()
+
+        return {'code': RET.OK, 'message': error_map[RET.OK], 'data': result}
+
+    except Exception as e:
+        return {'code': RET.UNKOWNERR, 'message': error_map[RET.UNKOWNERR], 'error': str(e)}
