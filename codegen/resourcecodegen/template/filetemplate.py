268f382169e671d9180c654c5bca4831a71ae857
PigKnight <1401470134@qq.com>
2021-12-30 17:53:44 +0800

    生成的app/_init_文件修改配置读入方式

--- codegen/resourcecodegen/template/filetemplate.py
+++ codegen/resourcecodegen/template/filetemplate.py
@@ -160,7 +160,7 @@ class {className}OtherResource(Resource):
 from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from .setting import Settings
+from . import setting
 
 # 数据库
 db = SQLAlchemy()
@@ -170,13 +170,20 @@ db = SQLAlchemy()
 def create_app(config_name):
     \"\"\"
     创建flask的应用对象
-    :param config_name: string 配置模式的名字  （"develop", "product"）
+    :param config_name: string 配置模式的名字  （"develop", "product", "test"）
     :return:
     \"\"\"
+    
+    config_mode = ｛
+        'develop': 'DevelopSettings',
+        'product': 'ProductSettings',
+        'test': 'TestSettings'
+    ｝
+    
     app = Flask(__name__)
 
     # 根据配置模式的名字获取配置参数的类
-    app.config.from_object(Settings)
+    app.config.from_object(getattr(setting, config_mode[config_name]))
 
     # 使用app初始化db
     db.init_app(app)
