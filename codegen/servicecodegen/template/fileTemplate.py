e6c72821118e1be0775dc2c8e35fb2ad7267893d
Nathan <wenzhustudy@gmail.com>
2022-01-05 21:17:17 +0800

    service代码生成

--- codegen/servicecodegen/template/fileTemplate.py
+++ codegen/servicecodegen/template/fileTemplate.py
@@ -16,41 +16,43 @@ class FileTemplate(object):
         File template class for service layer code
     """
 
-    template = """#!/usr/bin/env python
+    table_template = """#!/usr/bin/env python
 # -*- coding:utf-8 -*-
-
 {imports}
 
 class {table_name_initials_upper}Service({table_name_initials_upper}Controller):
     pass
 """
 
-    select_template = """
-        # select
-        @classmethod
-        def select(cls, **kwargs):
-            try:
-                filter_List = []
-                if kwargs.get('{primary_key}'):
-                    filter_list.append(cls.{primary_key} == kwargs.get('{primary_key}'))
-                else:
-                    {get_filter_list}
-               
-                page = int(kwargs.get('Page',1))
-                size = int(kwargs.get('Size', 10))
-           
-                {model_lower}_info = db.session.query(cls).filter(*filter_list)
-               
-                count = {model_lower}_info.count()
-                pages = math.ceil(count / size)
-                {model_lower}_info = {model_lower}_info.limit(size).offset((page - 1) * size).all()
-       
-                results = commons.query_to_dict({model_lower}_info)
-                return {{'code': RET.OK, 'message': error_map_EN[RET.OK], 'totalCount': count, 'totalPage': pages, 'data': results}}
-                
-            except Exception as e:
-                loggings.exception(1, e)
-                return {{'code': RET.DBERR, 'message': error_map_EN[RET.DBERR], 'data': {{'error': str(e)}}}}
-            finally:
-                db.session.close()
+    view_template = """#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+{imports}
+
+class {table_name_initials_upper}Service(object):
+
+    @classmethod
+    def joint_query(cls, **kwargs):
+    
+        page = int(kwargs.get('Page', 1))
+        size = int(kwargs.get('Size', 10))
+
+        filter_list = []
+    
+{filter_conditions}
+        try:
+            {table_name_lower_case}_info = db.session.query(t_{original_view_name}).filter(*filter_list)
+            
+            count = {table_name_lower_case}_info.count()
+            pages = math.ceil(count / size)
+            {table_name_lower_case}_info = {table_name_lower_case}_info.limit(size).offset((page - 1) * size).all()
+            results = commons.query_to_dict({table_name_lower_case}_info)
+    
+            return {{'code': RET.OK, 'message': error_map_EN[RET.OK], 'totalCount': count, 'totalPage': pages, 'data': results}}
+    
+        except Exception as e:
+            loggings.exception(1, e)
+            return {{'code': RET.DBERR, 'message': error_map_EN[RET.DBERR], 'data': {{'error': str(e)}}}}
+            
+        finally:
+            db.session.close()
 """
