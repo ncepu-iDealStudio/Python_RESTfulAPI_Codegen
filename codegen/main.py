3f7a8d1967691c786f150330195cd00072beb395
LEGION-Y7000\Nathan <wenzhustudy@gmail.com>
2022-01-22 13:24:27 +0800

    目标代码路径不正确bug修复

--- codegen/main.py
+++ codegen/main.py
@@ -91,7 +91,7 @@ def start(table_config, session_id):
 
         loggings.info(1, "Api project code generation completed")
 
-        with open('logs/codegen_log.log', 'r', encoding='utf-8') as f:
+        with open('logs/codegen_log_' + str(session_id) + '.log', 'r', encoding='utf-8') as f:
             result = f.read()
 
         return {'code': RET.OK, 'message': error_map[RET.OK], 'data': result}
