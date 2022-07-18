2acd8254defa8072dc36ac7c5917df286ad9b727
QuaY <2310236318@qq.com>
2022-07-18 15:04:08 +0800

    apiversion请求点改为大驼峰

--- codegen/resourcecodegen/template/filetemplate.py
+++ codegen/resourcecodegen/template/filetemplate.py
@@ -319,7 +319,7 @@ from api_{apiversion}.apiVersionResource.apiVersionResource import ApiVersionRes
 
 api = Api(apiversion_blueprint)
 
-api.add_resource(ApiVersionResource, '/apiversion', endpoint='apiversion')  # 测试接口，获取当前接口的版本
+api.add_resource(ApiVersionResource, '/apiversion', endpoint='Apiversion')  # 测试接口，获取当前接口的版本
 """
 
     api_version_resource = """#!/usr/bin/env python
