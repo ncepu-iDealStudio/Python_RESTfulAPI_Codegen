b8bb08b6991e4ca62b990a63a6fa4c7eb1ac39f9
jackiex <xjg@ncepu.edu.cn>
2023-06-05 10:32:30 +0800

    添加其它不在层中的辅助文件生成的代码；

---
+++ codegenerate/otherfilecodegen/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# file:__init__.py.py
+# author:jackiex
+# datetime:2023/6/5 10:29
+# software: PyCharm
+
+'''
+    this is function  description 
+'''
+# import module your need
