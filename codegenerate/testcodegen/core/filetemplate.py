b43f199259b4cce4ca345040f4f9f6799452469c
jackiex <xjg@ncepu.edu.cn>
2024-04-15 17:55:38 +0800

    更新模板

--- codegenerate/testcodegen/core/filetemplate.py
+++ codegenerate/testcodegen/core/filetemplate.py
@@ -30,8 +30,10 @@ class FileTemplate(object):
 import os
 import pytest
 
-from test.unitest.common.function_test_util import FunctionTestUtil
-from test.unitest.utils.read_yaml import read_yaml
+from test.unit_test.common.decorators import allure_decorate
+from test.unit_test.common.request_util import RequestUtil
+from test.unit_test.common.function_test_util import FunctionTestUtil
+from test.unit_test.tools.read_yaml import read_yaml
 
 base_url = os.path.dirname(os.path.abspath(__file__))
 
@@ -43,29 +45,36 @@ class Test{table_name}Service:
         """
 
     test_resource = """import os
+import allure
 import pytest
 
-from test.unitest.common.request_util import RequestUtil
-from test.unitest.utils.read_yaml import read_yaml
+from test.unit_test.common.decorators import allure_decorate
+from test.unit_test.common.request_util import RequestUtil
+from test.unit_test.utils.read_yaml import read_yaml
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
-
+@pytest.mark.resource
+@allure.feature("XXXX接口")
 class Test{table_name}Resource:
 
     @pytest.mark.parametrize("caseInfo", read_yaml(os.path.join(base_dir, "data/get.yaml")))
+    @allure_decorate("注释信息")
     def test_get(self, caseInfo, env_dict):
         RequestUtil.test_body(caseInfo, env_dict)
 
     @pytest.mark.parametrize("caseInfo", read_yaml(os.path.join(base_dir, "data/post.yaml")))
+    @allure_decorate("注释信息")
     def test_post(self, caseInfo, env_dict):
         RequestUtil.test_body(caseInfo, env_dict)
 
     @pytest.mark.parametrize("caseInfo", read_yaml(os.path.join(base_dir, "data/put.yaml")))
+    @allure_decorate("注释信息")
     def test_update(self, caseInfo, env_dict):
         RequestUtil.test_body(caseInfo, env_dict)
 
     @pytest.mark.parametrize("caseInfo", read_yaml(os.path.join(base_dir, "data/delete.yaml")))
+    @allure_decorate("注释信息")
     def test_delete(self, caseInfo, env_dict):
         RequestUtil.test_body(caseInfo, env_dict)
         """
